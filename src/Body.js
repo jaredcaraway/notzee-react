929d2312e58ce4b4f72c864b96070e68fffe4992
Jared Caraway <jared.caraway@gmail.com>
2018-07-12 12:23:58 -0500

    Factor out Body component

---
+++ src/Body.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Body.css';
+
+export default class Body extends React.Component {
+  render() {
+    return (
+      <p className="App-intro">
+        To get started, edit <code>src/App.js</code> and save to reload.
+      </p>
+    );
+  }
+}
