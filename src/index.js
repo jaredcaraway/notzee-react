8bb00768331f3031b7e33a60d64d80121165eff5
Jared Caraway <jared.caraway@gmail.com>
2023-03-22 22:02:28 -0500

    Remove service worker

--- src/index.js
+++ src/index.js
@@ -2,7 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import Game from './Game';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<Game />, document.getElementById('root'));
-registerServiceWorker();
